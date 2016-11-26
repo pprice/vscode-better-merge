38a83e1a765dbdd54009b3265aec5ce46be505d9
Phil Price <pprice@microsoft.com>
2016-11-25 19:27:45 -0800

    Move codelens provider into own class

---
+++ src/codelens-provider.ts
@@ -0,0 +1,66 @@
+
+import * as vscode from 'vscode';
+import MergeConflictParser, { DocumentMergeConflict } from './merge-conflict-parser';
+
+export default class MergeConflictCodeLensProvider implements vscode.CodeLensProvider, vscode.Disposable {
+
+    private disposables: vscode.Disposable[] = [];
+
+    constructor(private context: vscode.ExtensionContext) {
+
+    }
+
+    begin() {
+        this.disposables.push(
+            vscode.languages.registerCodeLensProvider({ pattern: '**/*' }, this),
+            vscode.commands.registerTextEditorCommand('better-merge.accept.ours', (editor, edit, ...args) => {
+                if (!args[0]) { return; }
+
+                const conflict: DocumentMergeConflict = args[0];
+                conflict.commitOursEdit(editor, edit);
+            }),
+            vscode.commands.registerTextEditorCommand('better-merge.accept.theirs', (editor, edit, ...args) => {
+                if (!args[0]) { return; }
+
+                const conflict: DocumentMergeConflict = args[0];
+                conflict.commitTheirsEdit(editor, edit);
+            })
+        );
+    }
+
+    dispose() {
+        if(this.disposables) {
+            this.disposables.forEach(disposable => disposable.dispose());
+            this.disposables = null;
+        }
+    }
+
+    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] {
+        if (!MergeConflictParser.containsConflict(document)) {
+            return null;
+        }
+
+        let items: vscode.CodeLens[] = [];
+        MergeConflictParser.scanDocument(document).forEach(conflict => {
+            let acceptOursCommand: vscode.Command = {
+                command: 'better-merge.accept.ours',
+                title: `Accept Our Change`,
+                arguments: [conflict]
+            };
+
+            let acceptTheirsCommand: vscode.Command = {
+                command: 'better-merge.accept.theirs',
+                title: `Accept Their Change`,
+                arguments: [conflict]
+            };
+            items.push(new vscode.CodeLens(conflict.range, acceptOursCommand));
+            items.push(new vscode.CodeLens(conflict.range, acceptTheirsCommand));
+        });
+
+        return items;
+    }
+
+    resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.CodeLens {
+        return;
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 import * as vscode from 'vscode';
 import MergeDecoratorService from './merge-decorator-service';
-import MergeConflictParser, { DocumentMergeConflict } from './merge-conflict-parser';
+import CodeLensProvider from './codelens-provider';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -9,60 +9,17 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Listen for active editor changes
     const mergeDecorator = new MergeDecoratorService(context);
-
+    const codeLensProvider = new CodeLensProvider(context);
     mergeDecorator.begin();
+    codeLensProvider.begin();
 
-    // Register for everything :|
+    // Register disposables
     context.subscriptions.push(
         mergeDecorator,
-        vscode.languages.registerCodeLensProvider({ pattern: '**/*' }, new CLP()),
-        vscode.commands.registerTextEditorCommand('better-merge.accept.ours', (editor, edit, ...args) => {
-            if(!args[0]) { return; }
-
-            const conflict : DocumentMergeConflict = args[0];
-            conflict.commitOursEdit(editor, edit);
-        }),
-        vscode.commands.registerTextEditorCommand('better-merge.accept.theirs', (editor, edit, ...args) => {
-            if(!args[0]) { return; }
-
-            const conflict : DocumentMergeConflict = args[0];
-            conflict.commitTheirsEdit(editor, edit);
-        })
+        codeLensProvider
     );
 }
 
 export function deactivate() {
 }
 
-class CLP implements vscode.CodeLensProvider {
-    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] {
-        if (!MergeConflictParser.containsConflict(document)) {
-            return null;
-        }
-
-        let items: vscode.CodeLens[] = [];
-        MergeConflictParser.scanDocument(document).forEach(conflict => {
-            let command: vscode.Command = {
-                command: 'better-merge.accept.ours',
-                title: `Accept Our Change`,
-                arguments: [conflict]
-            };
-
-            let command2: vscode.Command = {
-                command: 'better-merge.accept.theirs',
-                title: `Accept Their Change`,
-                arguments: [conflict]
-            };
-            items.push(new vscode.CodeLens(conflict.range, command));
-            items.push(new vscode.CodeLens(conflict.range, command2));
-        });
-
-        return items;
-    }
-
-    resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.CodeLens {
-        return;
-    }
-
-
-}
